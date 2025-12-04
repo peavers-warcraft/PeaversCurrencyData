a05d1d00c1bb84fcc32b8c01773037c632a663e2
Chris Turner <peavers@gmail.com>
2025-12-04 17:44:39 -0600

    Data update: currency-update-currencyrates-2025-12-04-234437

--- src/Data/CurrencyRates.lua
+++ src/Data/CurrencyRates.lua
@@ -1,9 +1,9 @@
 -- Auto-generated currency exchange rates
--- Last updated: 2025-12-04T20:46:45.969179401Z
+-- Last updated: 2025-12-04T23:44:37.724132675Z
 
 PeaversCurrencyData = PeaversCurrencyData or {}
 PeaversCurrencyData.CurrencyRates = {
-  lastUpdated = "2025-12-04T20:46:45.969179401Z",
+  lastUpdated = "2025-12-04T23:44:37.724132675Z",
 
   rates = {
     AUD = {
@@ -177,17 +177,17 @@ PeaversCurrencyData.CurrencyRates = {
   },
 
   symbols = {
-    HKD = "HK$",
-    CHF = "Fr",
-    KRW = "₩",
     CAD = "C$",
-    USD = "$",
-    GBP = "£",
-    NZD = "NZ$",
-    AUD = "A$",
-    CNY = "¥",
-    JPY = "¥",
-    EUR = "€",
+    KRW = "₩",
+    CHF = "Fr",
+    HKD = "HK$",
     TWD = "NT$",
+    EUR = "€",
+    JPY = "¥",
+    CNY = "¥",
+    AUD = "A$",
+    NZD = "NZ$",
+    GBP = "£",
+    USD = "$",
   }
 }

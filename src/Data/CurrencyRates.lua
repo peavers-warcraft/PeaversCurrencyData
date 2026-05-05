915f00417e27965d35c3adb9943dbdbb68b4da44
Chris Turner <peavers@gmail.com>
2026-05-04 23:12:47 -0500

    Data update: currency-update-currencyrates-2026-05-05-041245

--- src/Data/CurrencyRates.lua
+++ src/Data/CurrencyRates.lua
@@ -1,9 +1,9 @@
 -- Auto-generated currency exchange rates
--- Last updated: 2026-05-05T01:12:44.591372584Z
+-- Last updated: 2026-05-05T04:12:45.525244489Z
 
 PeaversCurrencyData = PeaversCurrencyData or {}
 PeaversCurrencyData.CurrencyRates = {
-  lastUpdated = "2026-05-05T01:12:44.591372584Z",
+  lastUpdated = "2026-05-05T04:12:45.525244489Z",
 
   rates = {
     CHF = {
@@ -204,6 +204,8 @@ PeaversCurrencyData.CurrencyRates = {
   },
 
   symbols = {
+    CAD = "C$",
+    AUD = "A$",
     USD = "$",
     HKD = "HK$",
     KRW = "₩",
@@ -215,7 +217,5 @@ PeaversCurrencyData.CurrencyRates = {
     NZD = "NZ$",
     CNY = "¥",
     EUR = "€",
-    CAD = "C$",
-    AUD = "A$",
   }
 }

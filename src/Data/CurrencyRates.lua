f0a6d6504daf1f1ffe55e43721d8fdab06a5b6a1
Chris Turner <peavers@gmail.com>
2026-05-30 23:12:47 -0500

    Data update: currency-update-currencyrates-2026-05-31-041245

--- src/Data/CurrencyRates.lua
+++ src/Data/CurrencyRates.lua
@@ -1,9 +1,9 @@
 -- Auto-generated currency exchange rates
--- Last updated: 2026-05-31T01:12:44.841503694Z
+-- Last updated: 2026-05-31T04:12:45.837919064Z
 
 PeaversCurrencyData = PeaversCurrencyData or {}
 PeaversCurrencyData.CurrencyRates = {
-  lastUpdated = "2026-05-31T01:12:44.841503694Z",
+  lastUpdated = "2026-05-31T04:12:45.837919064Z",
 
   rates = {
     CHF = {
